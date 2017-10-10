a7bffe85bced875ae9c70254595644a7aaa8088a
Massah <fmassah@cj.com>
2017-10-09 17:14:37 -0700

    Create helloworld app and test

--- src/main/scala/com/seefaribacode/onboarding/App.scala
+++ src/main/scala/com/seefaribacode/onboarding/App.scala
@@ -1,15 +1,40 @@
 package com.seefaribacode.onboarding
 
-/**
- * @author ${user.name}
- */
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
-  }
 
+  class Hello(var greeting: String = "Hello") {
+    private var nameC: String = ""
+
+    def sayHello(name: String): Unit = {
+      this.nameC = name
+      val x = getMessage()
+      println(x)
+    }
+
+    def getMessage(): String = {
+      println(s"inside def, nameC: $nameC")
+      s"$greeting $nameC"
+    }
+  }
+}
+
+
+// Alternative
+object App2 {
+
+  class Hello(var greeting: String = "Hello") {
+    private var name: String = null
+    private lazy val message: String = s"$greeting $name" //TODO evaluated once declared
+
+    def sayHello(name: String): Unit = {
+      this.name = name
+      val x = this.name
+      println(s"name $name")
+      println(s"this.name $x")
+      println(message)
+    }
+
+    def getMessage = message
+
+  }
 }
